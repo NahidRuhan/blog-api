55ecc5ff8de7308ba947401e4b4f2a9f18f49de7
NahidRuhan <dilal328@gmail.com>
2026-06-23 03:27:26 +0600

    setting up schema

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 app.use(cors({
@@ -12,7 +13,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/", async(req: Request, res: Response) => {
+    const user = await prisma.user.findMany()
+    console.log(user)
     res.send("Hello World!");
 })
 
